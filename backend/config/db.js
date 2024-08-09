b4b6827d5712cf173566d8aef4fa61f5b6e0fc36
Yashaswini_Raj <yashaswiniraj0892@gmail.com>
2024-08-09 09:03:10 +0530

    mongoose updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 dotenv.config();
-
+mongoose.set('strictQuery', false); 
 export const connectDB=async()=>{
     await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log("DB connected"))
